db4360ee84153695656a486d871e919e0fe272cf
Muhamadirfanm134 <35161809+Muhamadirfanm134@users.noreply.github.com>
2022-09-18 03:02:46 +0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", ".less"],
   },
   plugins: [
     new HtmlWebpackPlugin({
